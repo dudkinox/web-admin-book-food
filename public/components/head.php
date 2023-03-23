eb9d85019190a057d8c4aefb263abe55708edc5b
กีตาร์เอาเอง <guitar@odds.team>
2023-03-24 04:25:46 +0700

    :guitar: start project

---
+++ public/components/head.php
@@ -0,0 +1,11 @@
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>KYP Vietnamese Food Admin</title>
+
+<link rel="stylesheet" href="assets/css/main/app.css" />
+<link rel="stylesheet" href="assets/css/main/app-dark.css" />
+<link rel="shortcut icon" href="assets/images/logo/favicon.svg" type="image/x-icon" />
+<link rel="shortcut icon" href="assets/images/logo/favicon.png" type="image/png" />
+
+<link rel="stylesheet" href="assets/css/shared/iconly.css" />
+<link rel="stylesheet" href="css/main.css" />
---
+++ public/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'components/head.php'; ?>
+</head>
+
+<body>
+    <script src="assets/js/initTheme.js"></script>
+
+    <div id="app">
+        <?php include 'components/menu-pages.php'; ?>
+    </div>
+
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/app.js"></script>
+    <script src="assets/extensions/apexcharts/apexcharts.min.js"></script>
+    <script src="assets/js/pages/dashboard.js"></script>
+</body>
+
+</html>
