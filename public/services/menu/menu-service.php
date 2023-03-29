e992b83249713e5fa3780c5872dd6b00fe4e384d
กีตาร์เอาเอง <guitar@odds.team>
2023-03-30 02:05:37 +0700

    Update menu-service.php

--- public/services/menu/menu-service.php
+++ public/services/menu/menu-service.php
@@ -75,6 +75,33 @@ function findMenu($id)
 
 function editMenu($id, $calories, $image, $name, $price, $page)
 {
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://api-kyp.onrender.com/api/menus/' . $id,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => '{
+        "calories": ' . $calories . ',
+        "image": "' . $image . '",
+        "name": "' . $name . '",
+        "price": ' . $price . ',
+        "quantity": 0
+    }',
+        CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json'
+        ),
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    echo $response;
 
     echo "<script>window.location.href='?page=$page'</script>";
 }
